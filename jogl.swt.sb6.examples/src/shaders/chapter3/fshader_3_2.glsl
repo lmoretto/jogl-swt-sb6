3a5d1d240060d981c4f274985d2e069c6219e844
Luca Moretto <l.moretto88@gmail.com>
2013-12-28 18:01:15 +0100

    Added chapter3 page 30 example

---
+++ jogl.swt.sb6.examples/src/shaders/chapter3/fshader_3_2.glsl
@@ -0,0 +1,12 @@
+#version 430 core
+
+// Input from the vertex shader 
+in vec4 vs_color;
+
+// Output to the frame buffer
+out vec4 color;
+
+void main(void) {
+	// Simply assign the color we were given by the vertex shader to our output
+	color = vs_color;
+}
