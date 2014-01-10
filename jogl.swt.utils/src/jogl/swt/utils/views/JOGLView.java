1a85b936083746737743631f6b93a9d4cc214c3e
Luca Moretto <l.moretto88@gmail.com>
2014-01-10 12:02:32 +0100

    JOGLView: added camera controls
    mouse wheel: zoom
    arrows (alone): rotate camera (sphere space keeping the lookAt target fixed)
    ctrl + arrows: move camera (change lookAt target, maintain distance)

--- jogl.swt.utils/src/jogl/swt/utils/views/JOGLView.java
+++ jogl.swt.utils/src/jogl/swt/utils/views/JOGLView.java
@@ -20,7 +20,15 @@ import javax.media.opengl.GLEventListener;
 import javax.media.opengl.GLProfile;
 import javax.media.opengl.glu.GLU;
 
+import jogl.swt.utils.math.GLView;
+import jogl.swt.utils.math.Matrix4f;
+import jogl.swt.utils.math.Vec3f;
+
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.KeyListener;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseWheelListener;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
@@ -33,8 +41,6 @@ import org.eclipse.ui.part.ViewPart;
 import com.jogamp.opengl.swt.GLCanvas;
 
 public abstract class JOGLView extends ViewPart implements GLEventListener{
-	private long initTime;
-	
 	public static enum ShaderType {
 		VERTEX_SHADER(GL_VERTEX_SHADER, "Vertex shader"),
 		FRAGMENT_SHADER(GL_FRAGMENT_SHADER, "Fragment shader"),
@@ -59,6 +65,21 @@ public abstract class JOGLView extends ViewPart implements GLEventListener{
 		private String description;
 	}
 	
+	public static enum ProjectionType {
+		// ORTOGRAPHIC,
+		PERSPECTIVE
+	}
+	
+	private long initTime;
+	
+	private Matrix4f perspective;
+	
+	private Vec3f camera;
+	private Vec3f target;
+	private Vec3f up;
+	private int lastAngleUpDown;
+	private int lastAngleLeftRight;
+	private Matrix4f lookAt;
 	
 	private GLCanvas glCanvas;
 	
@@ -93,6 +114,16 @@ public abstract class JOGLView extends ViewPart implements GLEventListener{
 	protected long getApplicationTime() {
 		return System.currentTimeMillis() - initTime;
 	}
+	
+	protected Matrix4f getDefaultProjectionMatrix(ProjectionType type) {
+		if (type == ProjectionType.PERSPECTIVE)
+			return perspective;
+		return null;
+	}
+	
+	protected Matrix4f getLookAtMatrix() {
+		return lookAt;
+	}
 
 	@Override
 	public void createPartControl(Composite parent) {
@@ -108,6 +139,8 @@ public abstract class JOGLView extends ViewPart implements GLEventListener{
 		glCanvas = new GLCanvas(parent, SWT.NONE, caps, null);
 		glCanvas.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1));
 		glCanvas.addGLEventListener(this);
+		glCanvas.addKeyListener(keyListener);
+		glCanvas.addMouseWheelListener(mouseWheelListener);
 		
 		Composite buttonsContainer = new Composite(parent, SWT.NONE);
 		buttonsContainer.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, true, false, 1, 1));
@@ -203,6 +236,9 @@ public abstract class JOGLView extends ViewPart implements GLEventListener{
 	public void init(GLAutoDrawable drawable) {
 		initTime = System.currentTimeMillis();
 		
+		initLookAtParameters();
+		buildLookAt();
+		
 		System.out.println(drawable.getGL().getContext().getGLVersion());
 		if(!drawable.getGL().isGL4())
 			throw new RuntimeException("OpenGL 4 NOT supported on this machine");
@@ -219,6 +255,9 @@ public abstract class JOGLView extends ViewPart implements GLEventListener{
 	@Override
 	public void reshape(GLAutoDrawable drawable, int x, int y, int width,
 			int height) {
+		float aspect = (float) width / (float) height;
+		perspective = GLView.perspective(50.0f, aspect, 0.1f, 1000.0f);
+		
 		resize((GL4) drawable.getGL(), x, y, width, height);
 	}
 	
@@ -364,5 +403,198 @@ public abstract class JOGLView extends ViewPart implements GLEventListener{
 			}
 		}
 	}
+	
+	private final KeyListener keyListener = new KeyListener() {
+		
+		@Override
+		public void keyPressed(KeyEvent e) {
+			boolean ctrlPressed = (e.stateMask & SWT.CTRL) != 0;
+			
+			Vec3f direction = target.subtract(camera);
+			float distanceToTarget = direction.length();
+			
+			switch (e.keyCode) {
+			case SWT.ARROW_UP:
+				if(ctrlPressed) {
+					translateCameraUpDown(true);
+				}
+				else {
+					rotateCameraUp(distanceToTarget);
+				}
+				buildLookAt();
+				break;
+			case SWT.ARROW_DOWN:
+				if(ctrlPressed) {
+					translateCameraUpDown(false);
+				}
+				else {
+					rotateCameraDown(distanceToTarget);
+				}
+				buildLookAt();
+				break;
+			case SWT.ARROW_LEFT:
+				if(ctrlPressed) {
+					translateCameraRightLeft(false);
+				}
+				else {
+					rotateCameraLeft(distanceToTarget);
+				}
+				buildLookAt();
+				break;
+			case SWT.ARROW_RIGHT:
+				if(ctrlPressed) {
+					translateCameraRightLeft(true);
+				}
+				else {
+					rotateCameraRight(distanceToTarget);
+				}
+				buildLookAt();
+				break;
+			default:
+				break;
+			}
+		}
+		
+		@Override
+		public void keyReleased(KeyEvent e) {
+		}
+	};
+	
+	private final MouseWheelListener mouseWheelListener = new MouseWheelListener() {
+
+		@Override
+		public void mouseScrolled(MouseEvent e) {
+			Vec3f direction = target.subtract(camera).normalize();
+			
+			if(e.count > 0)
+				camera = camera.add(direction.multiply(0.2f));
+			else
+				camera = camera.add(direction.multiply(-0.2f));
+			buildLookAt();
+		}
+	};
+	
+	private void translateCameraUpDown(boolean positive) {
+		Vec3f[] base = computeCamerBase();
+		
+		Vec3f upVec = base[2];
+		
+		if(positive) {
+			camera = camera.add(upVec.multiply(0.2f));
+			target = target.add(upVec.multiply(0.2f));
+		}
+		else {
+			camera = camera.add(upVec.multiply(-0.2f));
+			target = target.add(upVec.multiply(-0.2f));
+		}
+	}
+	
+	private void translateCameraRightLeft(boolean positive) {
+		Vec3f[] base = computeCamerBase();
+		
+		Vec3f sideVec = base[1];
+		
+		if(positive) {
+			camera = camera.add(sideVec.multiply(0.2f));
+			target = target.add(sideVec.multiply(0.2f));
+		}
+		else {
+			camera = camera.add(sideVec.multiply(-0.2f));
+			target = target.add(sideVec.multiply(-0.2f));
+		}
+	}
+
+	private void rotateCameraUp(float distanceToTarget) {
+		lastAngleUpDown += CAMERA_ROTATION_ANGLE_STEP;
+		if(lastAngleUpDown >= MAX_UP_DOWN)
+			lastAngleUpDown = MAX_UP_DOWN;
+		
+		Vec3f coordinates = cartesianFromSphere(distanceToTarget, lastAngleUpDown, lastAngleLeftRight);
+		
+		camera = target.add(coordinates);
+	}
+	
+	private void rotateCameraDown(float distanceToTarget) {
+		lastAngleUpDown -= CAMERA_ROTATION_ANGLE_STEP;
+		if(lastAngleUpDown <= MIN_UP_DOWN)
+			lastAngleUpDown = MIN_UP_DOWN;
+		
+		Vec3f coordinates = cartesianFromSphere(distanceToTarget, lastAngleUpDown, lastAngleLeftRight);
+		
+		camera = target.add(coordinates);
+	}
+	
+	private void rotateCameraLeft(float distanceToTarget) {
+		lastAngleLeftRight -= CAMERA_ROTATION_ANGLE_STEP;
+		
+		Vec3f coordinates = cartesianFromSphere(distanceToTarget, lastAngleUpDown, lastAngleLeftRight);
+		
+		camera = target.add(coordinates);
+	}
+	
+	private void rotateCameraRight(float distanceToTarget) {
+		lastAngleLeftRight += CAMERA_ROTATION_ANGLE_STEP;
+		
+		Vec3f coordinates = cartesianFromSphere(distanceToTarget, lastAngleUpDown, lastAngleLeftRight);
+		
+		camera = target.add(coordinates);
+	}
+	
+	private Vec3f cartesianFromSphere(float len, float theta, float phi) {
+		theta = 90.0f - theta;
+		
+		float coordZ = (float) (len * Math.sin(Math.toRadians(theta)) * Math.cos(Math.toRadians(phi)));
+		float coordX = (float) (len * Math.sin(Math.toRadians(theta)) * Math.sin(Math.toRadians(phi)));
+		float coordY = (float) (len * Math.cos(Math.toRadians(theta)));
+		
+		return new Vec3f(coordX, coordY, coordZ);
+	}
+	
+	private void buildLookAt() {
+		Vec3f[] base = computeCamerBase();
+		
+		Vec3f forward = base[0];
+		Vec3f sideVec = base[1];
+		Vec3f newUp = base[2];
+		
+		lookAt = new Matrix4f(
+				sideVec.x, sideVec.y, sideVec.z, 0,
+				newUp.x, newUp.y, newUp.z, 0,
+				-forward.x, -forward.y, -forward.z, 0,
+				0, 0, 0, 1);
+		
+		lookAt = lookAt.mult(GLView.translate(-camera.x, -camera.y, -camera.z));
+	}
+	
+	private Vec3f[] computeCamerBase() {
+		Vec3f forward = target.subtract(camera).normalize();
+		
+		Vec3f sideVec = forward.cross(up);
+		
+		if(sideVec.length() == 0) {
+			//forward and up are coplanar
+			Vec3f tmpUp = new Vec3f(up.x, up.y, up.z - 0.0001f);
+			sideVec = forward.cross(tmpUp);
+		}
+		sideVec = sideVec.normalize();
+		
+		Vec3f newup = sideVec.cross(forward).normalize();
+		
+		Vec3f[] ret = new Vec3f[] {forward, sideVec, newup};
+		return ret;
+	}
+	
+	private void initLookAtParameters() {
+		float distance = 4.0f;
+		target = new Vec3f(0, 0, 0);
+		lastAngleUpDown = 0;
+		lastAngleLeftRight = 0;
+		camera = cartesianFromSphere(distance, lastAngleUpDown, lastAngleLeftRight);
+		up = new Vec3f(0, 1, 0);
+	}
+	
+	private static final int CAMERA_ROTATION_ANGLE_STEP = 5;
+	private static final int MIN_UP_DOWN = -89;
+	private static final int MAX_UP_DOWN = 89;
 
 }
